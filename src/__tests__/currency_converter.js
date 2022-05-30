c57fd9286474335786e25b59ce74d5dad8dd2a34
Antti Hätinen <antti.hatinen@phz.fi>
2022-05-30 13:57:04 +0300

    More tests

--- src/__tests__/currency_converter.js
+++ src/__tests__/currency_converter.js
@@ -6,8 +6,31 @@ test('convert null value should throw Error', () => {
     expect(() => { currencyConverter.convert(null, 0) } ).toThrow(Error);
 });
 
+test('convert undefined value should throw Error', () => {
+    expect(() => { currencyConverter.convert(undefined, undefined) } ).toThrow(Error);
+    expect(() => { currencyConverter.convert('USD', undefined) } ).toThrow(Error);
+    expect(() => { currencyConverter.convert(undefined, 0) } ).toThrow(Error);
+});
+
+
+test('convert negative values should work', () => {
+    expect(currencyConverter.convert('USD', -1) ).toBeCloseTo(-1.07, 1);
+});
+
+test('converting octal numbers would work normally', () => {
+    expect(currencyConverter.convert('USD', '08') ).toBe(8.56);
+});
+
+
+test('convert 32bit unsigned int max EUR to RUB', () => {
+    expect(currencyConverter.convert('RUB', 4294967295) ).toBe(307863255705.60004);
+});
+
+
+
+
 test('EUR to USD should be 1.07', () => {
-    expect(currencyConverter.convert('USD', 1)).toBe(1.07);
+    expect(currencyConverter.convert('USD', 1)).toBeCloseTo(1.07, 1);
 });
 
 
